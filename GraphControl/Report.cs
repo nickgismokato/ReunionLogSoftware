7dd925ce5eb51107dab038b16152c25385ce48e9
nickgismokato <nickvillumlaursen@gmail.com>
2023-04-14 16:15:15 +0200

    Added enum for Report masterData query

---
+++ GraphControl/Report.cs
@@ -0,0 +1,23 @@
+
+namespace ReunionLogSoftware.ObjectsAPI{
+    public enum ReportArguments{
+        endTime,
+        guildID,
+        guildName,
+        guildServerSlug,
+        guildServerRegion,
+        guildTagID,
+        userID,
+        limit,
+        page,
+        startTime,
+        zoneID,
+        gameZoneID
+    }
+    public class Report{
+
+        public Report(){
+
+        }
+    }
+}
